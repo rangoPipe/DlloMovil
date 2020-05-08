4f7ec401a8922bb670f371ddcf4678c0787580e3
rangoPipe <farango@ideassimples.com.co>
2020-05-07 21:57:41 -0500

    Se sube taller 4

---
+++ Taller4/MainActivity.cs
@@ -0,0 +1,63 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Logic.Manager;
+using Logic.Model;
+
+namespace Taller3
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            EditText txtId = FindViewById<EditText>(Resource.Id.txtId);
+
+            Button btnSearch = FindViewById<Button>(Resource.Id.btnSearch);
+            Button btnInsert = FindViewById<Button>(Resource.Id.btnInsert);
+
+            btnSearch.Click += (sender, args) =>
+            {
+                try
+                {
+                    ConnectionClass Manager = new ConnectionClass();
+                    PersonClass person = Manager.GetById(int.Parse(txtId.Text));
+                    Toast.MakeText(this, (person != null)? $"Se encontro con ID {person.Id.ToString()}": "Dicho ID no existe", ToastLength.Long).Show();
+                    
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                }
+            };
+
+            btnInsert.Click += (sender, args) =>
+            {
+                try
+                {
+                    ConnectionClass Manager = new ConnectionClass();
+                    PersonClass person = new PersonClass() { Id = int.Parse(txtId.Text) };
+                    Manager.SaveChanges(person);
+                    Toast.MakeText(this, "Se almaceno correctamente", ToastLength.Long).Show();
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                }
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Taller4/Manager/ConnectionClass.cs
@@ -0,0 +1,69 @@
+﻿using Logic.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Manager
+{
+    public class ConnectionClass
+    {
+        static object locker = new object();
+        SQLiteConnection connection;
+
+        public SQLiteConnection Connect()
+        {
+            try
+            {
+                string fileName = "SqlLiteApp.db3";
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                string fullPath = Path.Combine(path, fileName);
+                var data = new SQLiteConnection(fullPath);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public PersonClass GetById(int idPerson)
+        {
+            try
+            {
+                lock (locker)
+                {
+                    return connection.Table<PersonClass>().FirstOrDefault(x => x.Id == idPerson);
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public int SaveChanges(PersonClass person)
+        {
+            try
+            {
+                lock (locker)
+                {
+                    return connection.Insert(person);
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public ConnectionClass()
+        {
+            connection = Connect();
+            connection.CreateTable<PersonClass>();
+        }
+    }
+}
